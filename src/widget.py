2d9d1cdad47c34e7848759e24612f0f5245b33d7
MaksimVolkov <skazimko@yandex.ru>
2024-07-01 11:36:47 +0300

    update masks.py, widget.py for PEP8

--- src/widget.py
+++ src/widget.py
@@ -1,3 +1,15 @@
-def mask_account_card(user_info: str) -> str:
-    if len(user_info) == 24:
-        
+from src.masks import get_mask_card_number
+from src.masks import get_mask_account
+
+
+def mask_account_card(user_info: str) -> str | None:
+    """Функция маскирующая номер карты или счета."""
+    if "Счет" in user_info:
+        return get_mask_account(user_info)
+    else:
+        return get_mask_card_number(user_info)
+
+
+def get_data(data_info: str) -> str:
+    """Функция преобразования даты"""
+    return f"{data_info[8:10]}.{data_info[5:7]}.{data_info[:4]}"
