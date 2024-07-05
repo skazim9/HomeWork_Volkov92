19af9c7edfe62683ac24af36e0c3f6a697b2ad32
MaksimVolkov <skazimko@yandex.ru>
2024-07-05 12:01:45 +0300

    create function sort_by_date

--- src/processing.py
+++ src/processing.py
@@ -7,3 +7,12 @@ def filter_by_state(list_of_dict: list[dict[str, any]], state: str = "EXECUTED")
     у которых ключ state соответствует указанному значению.
     '''
     return [new_dict for new_dict in list_of_dict if new_dict.get("state") == state]
+
+
+def sort_by_date(list_of_dict: list[dict[str, Any]], reverse: bool = True) -> list[dict[str, Any]]:
+    """
+    Функция принимает на вход список словарей и возращает новый отсортированный,
+    по убыванию даты.
+    """
+    sorted_list = sorted(list_of_dict, key=lambda new_list_of_dict: new_list_of_dict["date"], reverse=reverse)
+    return sorted_list
