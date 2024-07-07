3707159e6f81ae71474750c6b37155f07b2760d4
MaksimVolkov <skazimko@yandex.ru>
2024-07-07 22:27:34 +0300

    update processing.py , README.md

--- src/processing.py
+++ src/processing.py
@@ -1,7 +1,7 @@
 from typing import Any
 
 
-def filter_by_state(list_of_dict: list[dict[str, any]], state: str = "EXECUTED") -> any:
+def filter_by_state(list_of_dict: list[dict[str, Any]], state: str = "EXECUTED") -> Any:
     '''
     Функция принимает список словарей и возвращает новый список словарей,
     у которых ключ state соответствует указанному значению.
@@ -9,10 +9,10 @@ def filter_by_state(list_of_dict: list[dict[str, any]], state: str = "EXECUTED")
     return [new_dict for new_dict in list_of_dict if new_dict.get("state") == state]
 
 
-def sort_by_date(list_of_dict: list[dict[str, Any]], reverse: bool = True) -> list[dict[str, Any]]:
+def sort_by_date(list_of_dict: list[dict[str, Any]], is_reverse: bool = True) -> list[dict[str, Any]]:
     """
     Функция принимает на вход список словарей и возращает новый отсортированный,
     по убыванию даты.
     """
-    sorted_list = sorted(list_of_dict, key=lambda new_list_of_dict: new_list_of_dict["date"], reverse=reverse)
+    sorted_list = sorted(list_of_dict, key=lambda new_list_of_dict: new_list_of_dict["date"], reverse=is_reverse)
     return sorted_list
