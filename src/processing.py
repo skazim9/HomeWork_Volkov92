e872b98c21292158df363d34a9337817b6cc0132
MaksimVolkov <skazimko@yandex.ru>
2024-07-09 10:21:12 +0300

    update processing.py

--- src/processing.py
+++ src/processing.py
@@ -2,10 +2,10 @@ from typing import Any
 
 
 def filter_by_state(list_of_dict: list[dict[str, Any]], state: str = "EXECUTED") -> Any:
-    '''
+    """
     Функция принимает список словарей и возвращает новый список словарей,
     у которых ключ state соответствует указанному значению.
-    '''
+    """
     return [new_dict for new_dict in list_of_dict if new_dict.get("state") == state]
 
 
